2de966b20ce48947b44eeda26ddae5e01d7094da
FluffyPenguin <alexkimt@gmail.com>
2020-07-21 23:31:54 +0000

    Fix app.module.ts imports

--- marketplace/src/app/app.module.ts
+++ marketplace/src/app/app.module.ts
@@ -4,15 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LandingComponent } from './landing/landing.component';
-import { UserActionsComponent } from './user-actions/user-actions.component';
-import { SellItemComponent } from './sell-item/sell-item.component';
-import { BuyComponent } from './buy/buy.component';
-import { BuyItemComponent } from './buy-item/buy-item.component';
-import { ItemCardComponent } from './item-card/item-card.component';
-import { UploadComponent } from './sell-item/upload/upload.component';
-import { DetailsComponent } from './sell-item/details/details.component';
-import { SubmitComponent } from './sell-item/submit/submit.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { UserActionsComponent } from './components/user-actions/user-actions.component';
+import { SellItemComponent } from './components/sell-item/sell-item.component';
+import { BuyItemComponent } from './components/buy-item/buy-item.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
+import { UploadComponent } from './components/sell-item/upload/upload.component';
+import { DetailsComponent } from './components/sell-item/details/details.component';
+import { SubmitComponent } from './components/sell-item/submit/submit.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,6 @@ import { SubmitComponent } from './sell-item/submit/submit.component';
     LandingComponent,
     UserActionsComponent,
     SellItemComponent,
-    BuyComponent,
     BuyItemComponent,
     ItemCardComponent,
     UploadComponent,
