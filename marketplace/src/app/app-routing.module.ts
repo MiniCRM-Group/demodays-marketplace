977089382de19bba0bb55c1d6c423a4ef172520e
FluffyPenguin <alexkimt@gmail.com>
2020-07-22 00:26:36 +0000

    Add routes in app-routing.module

--- marketplace/src/app/app-routing.module.ts
+++ marketplace/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserActionsComponent } from './components/user-actions/user-actions.component';
+import { SellItemComponent } from './components/sell-item/sell-item.component';
+import { BuyItemComponent } from './components/buy-item/buy-item.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: UserActionsComponent },
+  { path: 'sell', component: SellItemComponent },
+  { path: 'buy', component: BuyItemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
