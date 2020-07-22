b54a358d4b790ca49134d957d6f4f99b00baa6d5
FluffyPenguin <alexkimt@gmail.com>
2020-07-22 18:41:42 +0000

    Add routing to sell child components

--- marketplace/src/app/app-routing.module.ts
+++ marketplace/src/app/app-routing.module.ts
@@ -4,11 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserActionsComponent } from './components/user-actions/user-actions.component';
 import { SellItemComponent } from './components/sell-item/sell-item.component';
 import { BuyItemComponent } from './components/buy-item/buy-item.component';
+import { UploadComponent } from './components/sell-item/upload/upload.component';
+import { DetailsComponent } from './components/sell-item/details/details.component';
+import { SubmitComponent } from './components/sell-item/submit/submit.component';
 
 
 const routes: Routes = [
   { path: '', component: UserActionsComponent },
-  { path: 'sell', component: SellItemComponent },
+  { path: 'sell', component: SellItemComponent,
+    children: [
+      { path:'upload', component: UploadComponent },
+      { path:'details', component: DetailsComponent },
+      { path:'submit', component: SubmitComponent },
+    ]},
   { path: 'buy', component: BuyItemComponent },
 ];
 
