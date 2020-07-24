07724c5c5f904d5f4808e55fb63c779502e5b5a1
FluffyPenguin <alexkimt@gmail.com>
2020-07-24 23:33:03 +0000

    Fix imports

--- marketplace/src/app/services/retrieve-item.service.ts
+++ marketplace/src/app/services/retrieve-item.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { ItemResponse, Item } from "../models/item";
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
